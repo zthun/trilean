414a1eff565df63fe0c0e6959561aaecd5e448e5
Anthony Bonta <zthun.coding@gmail.com>
2025-01-03 12:32:51 -0600

    build: refactor to use vite-plugin-external

--- .config/vite-library.ts
+++ .config/vite-library.ts
@@ -1,18 +1,32 @@
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
-import { externalizeDeps } from 'vite-plugin-externalize-deps';
+import { createRequire } from "node:module";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import external from "vite-plugin-external";
 
 export function defineLibrary(dir: string) {
+  const $require = createRequire(import.meta.url);
+  const pkg = $require(resolve(dir, "package.json"));
+
   return defineConfig({
     build: {
       lib: {
         entry: `${dir}/src/index.ts`,
-        formats: ['es', 'cjs'],
-        fileName: 'index'
+        formats: ["es", "cjs"],
+        fileName: "index",
       },
       minify: false,
-      sourcemap: true
+      sourcemap: true,
     },
-    plugins: [externalizeDeps(), dts({ tsconfigPath: `${dir}/tsconfig.prod.json` })]
+    plugins: [
+      external({
+        nodeBuiltins: true,
+        externalizeDeps: [
+          ...Object.keys(pkg.dependencies || {}),
+          ...Object.keys(pkg.devDependencies || {}),
+        ],
+      }),
+      dts({ tsconfigPath: `${dir}/tsconfig.prod.json` }),
+    ],
   });
 }
