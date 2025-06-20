4c7572a17fe20472041ccf327e7790a3d7b8a8e8
Anthony Bonta <zthun.coding@gmail.com>
2025-06-19 23:12:14 -0500

    build: update outdated packages

--- .config/janitorrc.mjs
+++ .config/janitorrc.mjs
@@ -4,22 +4,13 @@ import {
 } from "@zthun/janitor-options";
 
 const lint = new ZJanitorOptionsLintBuilder()
-  .esFile("*.{js,cjs,mjs,ts,mts}")
-  .esFile("packages/**/src/**/*.{js,cjs,mjs,ts,mts}")
-  .markdownFile("*.md")
-  .markdownFile("packages/**/*.md")
-  .jsonFile("*.json", "packages/**/*/json")
-  .yamlFile(".circleci/config.yml")
+  .commonEsFiles()
+  .commonMarkdownFiles()
+  .commonJsonFiles()
+  .commonYamlFiles()
   .generatePrettyFiles()
   .generateSpellingFiles()
-  .excludeAll("**/CHANGELOG.md")
-  .excludeAll("packages/**/dist/**")
-  .excludeAll("packages/**/docs/**")
-  .excludeAll("node_modules/**")
-  .excludeAll("packages/**/node_modules/**")
-  .excludeAll("package-lock.json")
-  .excludeAll(".yarnrc.yml")
-  .excludeAll("cspell.json")
-  .excludeAll("lerna.json")
+  .commonExcludes()
   .build();
+
 export default new ZJanitorOptionsBuilder().lint(lint).build();
