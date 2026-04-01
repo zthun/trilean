c79b9271da9de84a93b651fa27afb497b98c5e32
Anthony Bonta <zthun.coding@gmail.com>
2026-04-01 04:32:20 -0500

    build: join all config files into eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,3 @@
-import { environments, recommended } from "@zthun/janitor-lint-config/eslint";
+import { environments, recommended } from "@zthun/janitor-eslint-config";
 
 export default [...recommended, ...environments.node];
